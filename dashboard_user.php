fda3edb14ed5490f73a1340f50694e8f6e8779c0
Fatahkhairurrizqy <fatahkhairurrizqy@gmail.com>
2026-04-15 17:28:08 +0700

    first commit

--- dashboard_user.php
+++ dashboard_user.php
@@ -65,7 +65,7 @@ if(!isset($_SESSION['login']) || $_SESSION['role'] != 'user'){
 </div>
 
 <footer>
-    &copy; <?= date('Y') ?>  BY AYOMI ENJANG UTAMI - All Rights Reserved
+    &copy; <?= date('Y') ?>  BY FATAH KHAIRURRIZQY BUDIANSYAH - All Rights Reserved
 </footer>
 
 </body>
